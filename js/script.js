858560c8161d4a8825e70bee7be26958a62598f9
Shannon Mulloy <smulloy21@gmail.com>
2015-07-08 23:59:18 -0400

    stuck on bug in jquery

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
-	$(".group ul").on("click", "li", function(){
-		alert("cross me out!");
-	});
-	$(".title").on("click", function(event){
-		event.stopPropagation();
-		alert("cross me out!");
-	});
-	$("i").on("click", function(event){
-		event.stopPropagation();
-		alert("delete me!");
-	});
 	$(".group input").keypress(function(e) {
 		if (e.which == 13) {
-			alert("add item!");
+			$(this).siblings("ul").append('<li><p>' + $(this).val() + '</p><i class="fa fa-times-circle"></i></li>');
+			$(this).val('');
 		};
 	});
 	$("#add-group").keypress(function(e) {
 		if (e.which == 13) {
-			alert("add group!");
+			$(".groups").append('<div class="group"><div class="title"><h2>' + $(this).val() + '</h2><i class="fa fa-times-circle"></i></div><input type="text" placeholder="+ item" /><ul></ul></div>');
+			$(this).val('');
 		};
+	});
+	$("li i").on("click", function(event){
+		$(this).parent().remove();
+		alert("grrrr");
+	});
+	$(".title i").on("click", function(event){
+		$(this).parent().parent().remove();
+	});
+	$(".group ul").on("click", "li", function(){
+		$(this).toggleClass("crossed-out");
+	});
+	$(".title").on("click", function(event){
+		$(this).toggleClass("crossed-out");
 	});
 });
